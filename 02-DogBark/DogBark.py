89972b5dcb64a425bb5e095e3f6d2e848a0151c9
Aidaneyro <aidanwesterland638@gmail.com>
2025-06-19 11:38:03 -0400

    Just photo and text

--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -17,12 +17,18 @@ def main():
     pygame.display.set_caption("Text, Sound, and an Image")
 
     # Prepare the image
-    # TODO 1: Create an image with the 2dogs.JPG image
-    # TODO 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    # TODONE 1: Create an image with the 2dogs.JPG image
+    image1 = pygame.image.load("2dogs.JPG")
+
+    # TODONE 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    image1 = pygame.transform.scale(image1, (IMAGE_SIZE, IMAGE_SIZE))
 
     # Prepare the text caption(s)
-    # TODO 4: Create a font object with a size 28 font.
-    # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
+    # TODONE 4: Create a font object with a size 28 font.
+    font1 = pygame.font.SysFont("comicsans", 28)
+
+    # TODONE 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
+    caption1 = font1.render("Two Dogs", True, BLACK )
 
     # Prepare the music
     # TODO 8: Create a Sound object from the "bark.wav" file.
@@ -37,14 +43,20 @@ def main():
         screen.fill(WHITE)
 
         # Draw the image onto the screen
-        # TODO 2: Draw (blit) the image onto the screen at position (0, 0)
+        # TODONE 2: Draw (blit) the image onto the screen at position (0, 0)
+        screen.blit(image1, (0, 0))
 
         # Draw the text onto the screen
-        # TODO 6: Draw (blit) the text image onto the screen in the middle bottom.
+        # TODONE 6: Draw (blit) the text image onto the screen in the middle bottom.
         # Hint: Commands like these might be useful..
         #          screen.get_width(), caption1.get_width(), image1.get_height()
+        screen.blit(caption1, (image1.get_width()//2 - caption1.get_width()//2,
+                               image1.get_height() - 8))
 
-        # TODO 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        # TODONE 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        font2 = pygame.font.SysFont("comicsans", 40)
+        caption2 = font2.render("STAHP IT", True, WHITE )
+        screen.blit(caption2, (250, 20 ))
 
         # Update the screen
         pygame.display.update()
